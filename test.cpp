067c9a7d793b747384af0209823f2a1b17e4a2ac
Nathan louissaint <louissaintmark@gmail.com>
2023-04-04 17:56:44 -0400

    first c++ command

---
+++ test.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+// int main(){
+//     std::cout<<"hello world";
+// }
+
+int main (){
+int x = 20;
+int y = 30;
+int answer = y + x;
+std::cout<< answer;
+return 0;
+}
+
