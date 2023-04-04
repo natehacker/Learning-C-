34537977d2160976b71b1e37f5e0e58e6d6995cb
Nathan louissaint <louissaintmark@gmail.com>
2023-04-04 18:11:36 -0400

    more c++

---
+++ tempCodeRunnerFile.cpp
@@ -0,0 +1,7 @@
+int main (){
+// int x = 20;
+// int y = 30;
+// int answer = y + x;
+// std::cout<< answer;
+// return 0;
+// }
--- test.cpp
+++ test.cpp
@@ -1,14 +1,42 @@
 #include <iostream>
 using namespace std;
+//intergers
 // int main(){
 //     std::cout<<"hello world";
 // }
+// int main (){
+// int x = 20;
+// int y = 30;
+// int answer = y + x;
+// std::cout<< answer;
+// return 0;
+// }
+//creating strings
+// int main () {
+//    cout<<"hello world";
+//    cout<< "im learning c++";
+//     return 0;
+// }
+// strings on two different lines 
+// int main (){
+//     cout<<"hello\n";
+//     cout <<"Nathan";
+//     return 0;
+// }
+// // Variables in Cc++
+// int main(){
+// int myNum = 15;
+// cout<<myNum;
+// return 0;
+// }
 
+//user  numbe input 
 int main (){
-int x = 20;
-int y = 30;
-int answer = y + x;
-std::cout<< answer;
-return 0;
+    int x;
+    cout<< "type a number:";
+    cin>> x;
+    cout << "your number is "; 
+    return 0;
 }
+
 
